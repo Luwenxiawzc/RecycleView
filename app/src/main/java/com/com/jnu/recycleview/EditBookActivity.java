0de29c69c5ef67106a07c5761484eea98947acc3
ASUS <1520802410@qq.com>
2022-10-19 19:17:15 +0800

    第八次实验：数据回传和数据传递

---
+++ app/src/main/java/com/com/jnu/recycleview/EditBookActivity.java
@@ -0,0 +1,51 @@
+package com.com.jnu.recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditBookActivity extends AppCompatActivity {
+
+    public static final int RESULT_CODE_SUCCESS = 666;
+    private int position;//传入与传出当前位置
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book_activity);
+
+        position=this.getIntent().getIntExtra("position",0);//传入当前位置
+        EditText editTextTitle=findViewById(R.id.edittext_shop_item_title);
+
+        String title=this.getIntent().getStringExtra("title");//传入当前title
+        if(null!=title){
+            editTextTitle.setText(title);//将EditText的值修改为传过来的title
+        }//title为空是增加操作，不为空是更新操作
+
+        Button button_yes=findViewById(R.id.button_确定);
+        Button button_no=findViewById(R.id.button_取消);
+
+        button_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putString("title", editTextTitle.getText().toString());//传回新输入的title
+                bundle.putInt("position",position);//传回当前位置
+
+                intent.putExtras(bundle);
+                setResult(RESULT_CODE_SUCCESS, intent);
+                EditBookActivity.this.finish();//记得关闭当前的activity
+            }
+        });
+        button_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditBookActivity.this.finish();//选择取消按钮退出EditBookActivity
+            }
+        });
+    }
+}
--- app/src/main/java/com/com/jnu/recycleview/MainActivity.java
+++ app/src/main/java/com/com/jnu/recycleview/MainActivity.java
@@ -24,23 +24,39 @@ public class MainActivity extends AppCompatActivity {
     private static final int MENU_ID_ADD =1;
     private static final int MENU_ID_UPDATE =2;
     private static final int MENU_ID_DELETE =3;
-    public ArrayList<Book> books;
+    public ArrayList<Book> books;//Book列表
     private MainRecycleViewAdapter mainRecycleViewAdapter;
 
     private final ActivityResultLauncher<Intent> addDataLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result->{
-        if(null!=result){
-            Intent intent=result.getData();
-            if(result.getResultCode()!=InputShopItemActivity.RESULT_CODE_SUCCESS)
-            {
-                assert intent != null;
-                Bundle bundle=intent.getExtras();
-                String title=bundle.getString("title");
-                books.add(1, new Book(title,R.drawable.ic_launcher_background));
-                mainRecycleViewAdapter.notifyItemInserted(1);
-            }
-        }
-    } );
+                if(null!=result){
+                    Intent intent=result.getData();//获得传回的intent
+                    if(result.getResultCode()== EditBookActivity.RESULT_CODE_SUCCESS)
+                    {
+                        assert intent != null;
+                        Bundle bundle=intent.getExtras();
+                        String title=bundle.getString("title");//获得传回的新的title
+                        int position=bundle.getInt("position");//获得传回的当前数据的位置
+                        books.add(position, new Book(title,R.drawable.book_no_name));//添加一个新的Book
+                        mainRecycleViewAdapter.notifyItemInserted(position);//通知适配器数据增加
+                    }
+                }
+            } );//新的数据传递（add的数据回传）
+    private final ActivityResultLauncher<Intent> updateDataLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result->{
+                if(null!=result){
+                    Intent intent=result.getData();//获得传回的intent
+                    if(result.getResultCode()== EditBookActivity.RESULT_CODE_SUCCESS)
+                    {
+                        assert intent != null;
+                        Bundle bundle=intent.getExtras();
+                        String title=bundle.getString("title");//获得传回的新的title
+                        int position=bundle.getInt("position");//获得传回的当前数据的位置
+                        books.get(position).setTitle(title);//修改title为传回的新的title
+                        mainRecycleViewAdapter.notifyItemChanged(position);//通知适配器数据更改
+                    }
+                }
+            } );//新的数据传递（update的数据回传）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerViewMain = findViewById(R.id.recycle_view_books);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);//垂直
         recyclerViewMain.setLayoutManager(linearLayoutManager);
 
         books = new ArrayList<>();
@@ -72,12 +88,15 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId())
         {
             case MENU_ID_ADD:
-                Intent intent=new Intent(this, InputShopItemActivity.class);
-                addDataLauncher.launch(intent);
+                Intent intent=new Intent(this, EditBookActivity.class);
+                intent.putExtra("position",item.getOrder());//传递当前位置
+                addDataLauncher.launch(intent);//数据回传
                 break;
             case MENU_ID_UPDATE:
-                books.get(item.getOrder()).setTitle(getString(R.string.update));
-                mainRecycleViewAdapter.notifyItemChanged(item.getOrder());
+                Intent intentupdate=new Intent(this, EditBookActivity.class);
+                intentupdate.putExtra("position",item.getOrder());//传递当前位置
+                intentupdate.putExtra("title",books.get(item.getOrder()).getTitle());//传递当前的title
+                updateDataLauncher.launch(intentupdate);//数据回传
                 break;
             case MENU_ID_DELETE:
                 AlertDialog alertDialog=new AlertDialog.Builder(this)
@@ -88,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
                             mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
                         }).setNegativeButton(R.string.no, (dialog, which) -> {
                         }).create();
-                alertDialog.show();
+                alertDialog.show();//对话框
                 break;
         }
         return super.onContextItemSelected(item);
@@ -119,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
                 contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add "+getAdapterPosition());
                 contextMenu.add(0,MENU_ID_UPDATE,getAdapterPosition(),"Update "+getAdapterPosition());
                 contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete "+getAdapterPosition());
-            }
+            }//上下文菜单的设置
         }
 
         public MainRecycleViewAdapter(ArrayList<Book> dataSet) {
