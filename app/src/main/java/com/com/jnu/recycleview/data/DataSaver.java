92ab4c57edc6b0b53bbbc5a2a1b0f7990cc27748
ASUS <1520802410@qq.com>
2022-10-30 11:48:46 +0800

    实验9

--- app/src/main/java/com/com/jnu/recycleview/data/DataSaver.java
+++ app/src/main/java/com/com/jnu/recycleview/data/DataSaver.java
@@ -1,5 +1,4 @@
 package com.com.jnu.recycleview.data;
-
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -11,12 +10,11 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class DataSaver {
-    public void Save(Context context, ArrayList<Book> data)//序列化
+    public void Save(Context context, ArrayList<Book> data)//保存数据
     {
         try {
-
-            FileOutputStream dataStream=context.openFileOutput("mydata.dat",Context.MODE_PRIVATE);
-            ObjectOutputStream out = new ObjectOutputStream(dataStream);
+            FileOutputStream dataStream=context.openFileOutput("mydata.dat",Context.MODE_PRIVATE);//指定文件名称，不能包含路径分隔符“/” ，文件不存在会自动创建。
+            ObjectOutputStream out = new ObjectOutputStream(dataStream);//序列化
             out.writeObject(data);
             out.close();
             dataStream.close();
@@ -25,12 +23,12 @@ public class DataSaver {
         }
     }
     @NonNull//不会返回空
-    public ArrayList<Book> Load(Context context)//反序列化
+    public ArrayList<Book> Load(Context context)//获取数据
     {
         ArrayList<Book> data=new ArrayList<>();
         try {
             FileInputStream fileIn = context.openFileInput("mydata.dat");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
+            ObjectInputStream in = new ObjectInputStream(fileIn);//反序列化
             data = (ArrayList<Book>) in.readObject();
             in.close();
             fileIn.close();
